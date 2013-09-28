740fe86f26847f38f6d5a76eba24f951d87111c9
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2013-09-28 14:56:46 +0200

    Added physical motion to Mobile

--- Assets/Scripts/Objects/Mobiles/Mobile.cs
+++ Assets/Scripts/Objects/Mobiles/Mobile.cs
@@ -4,17 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using EventHorizonGame;
 
 public abstract class Mobile : MonoBehaviour
 {
     public string Name { get; protected set; }
+    protected Rect Size;
 
-    protected Vector3 Velocity = Vector3.zero;
-    protected float MaxSpeed = 10f;
-    protected float Acceleration = 0.9F;
-    protected bool isAccelerating = false;
-    protected float Inertia = 0.9F;
-    float currentSpeed = 0;
+    public MotionParameters motionParams;
 
     Vector3 lastPos;
 
@@ -22,36 +19,63 @@ public abstract class Mobile : MonoBehaviour
 
     public virtual void LoadDefaultModel()
     {
-        SetModel(Utils.Load<GameObject>("DefaultMobile"));
+        SetModel(Utils.Load<GameObject>("DefaultMobile"), Vector3.zero);
     }
 
-    public void SetModel(GameObject g)
+    public static Rect GetRectSize(GameObject model)
+    {
+        Bounds b = model.GetComponent<MeshRenderer>().bounds;
+        Rect size = new Rect(b.min.x, b.min.y, b.size.x, b.size.y);
+
+        return size;
+    }
+
+    public void SetModel(GameObject g, Vector3 position)
+    {
+        SetModel(g, position, true);
+    }
+
+    public void SetModel(GameObject g, Vector3 position, bool instantiate)
     {
         if (Model != null)
             GameObject.DestroyImmediate(Model);
 
-        Model = (GameObject)GameObject.Instantiate(g);
+        if (instantiate)
+            Model = (GameObject)GameObject.Instantiate(g);
+
+        else Model = g;
+
+        Model.transform.position = position;
         Model.gameObject.GetComponent<CollisionTester>().parent = this;
+        Size = GetRectSize(Model);
+
+        if (EventHorizon.Instance.USE_PLACEHOLDERS)
+        {
+            Model.renderer.sharedMaterial = EventHorizon.Instance.PLACEHOLDER;
+        }
     }
 
     public void Move(Vector3 normalizedDirection)
     {
-        Velocity += (normalizedDirection * Time.deltaTime * Acceleration);
-        Mathf.Clamp(Velocity.magnitude, 0, MaxSpeed);
+        motionParams.Velocity += (normalizedDirection * Time.deltaTime * motionParams.Acceleration);
+        Mathf.Clamp(motionParams.Velocity.magnitude, 0, motionParams.MaxSpeed);
     }
 
     public void Stop()
     {
-        Velocity = Vector3.zero;
+        motionParams.Velocity = Vector3.zero;
     }
 
     protected virtual void Update()
     {
-        Velocity *= Inertia;
-        //if (!isAccelerating)
-        //    Velocity.Scale( += (normalizedDirection * Time.deltaTime * Acceleration);
-        Model.transform.Translate(Velocity, Space.World);
-        lastPos = Model.transform.position;
+        if (Model != null)
+        {
+            motionParams.Velocity *= motionParams.Inertia;
+            Model.transform.Translate(motionParams.Velocity, Space.World);
+
+            lastPos = Model.transform.position;
+            DestroyWhenOutOfSpawnArea();
+        }
     }
 
     public override string ToString()
@@ -64,9 +88,22 @@ public abstract class Mobile : MonoBehaviour
         Debug.Break();
     }
 
-    public virtual void HitVoid()
+    void Awake()
     {
-        Debug.Log("HitVoid");
+        motionParams = new MotionParameters { MaxSpeed = 0, Inertia = 0F, Acceleration = 0F, Velocity = Vector3.zero, CurrentSpeed = 0 };
+    }
+
+    // Destroy the mobile when its rectangle is *totally* out of Spawn area.
+    private void DestroyWhenOutOfSpawnArea()
+    {
+        if (Model.transform.position.x < Globals.SpawnArea.x - Size.width / 2
+            || Model.transform.position.x > Globals.SpawnArea.x + Globals.SpawnArea.width + Size.width / 2
+            || Model.transform.position.y < Globals.SpawnArea.y - Size.height / 2
+            || Model.transform.position.y > Globals.SpawnArea.y + Globals.SpawnArea.height + Size.height / 2)
+        {
+            Destroy(Model);
+            Destroy(this);
+        }
     }
 
 
