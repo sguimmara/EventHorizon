00e8deafc04b41446c22055802b3c69629cd5601
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2013-09-28 14:53:55 +0200

    Added MotionParameters struct.
    This struct is used to store a mobile's motion data :
    - Velocity (Vector3)
    - Max speed (float)
    - Acceleration (float)
    - Inertia (float)
    - CurrentSpeed (float)

---
+++ Assets/Scripts/Objects/Mobiles/MotionParameters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace EventHorizonGame
+{
+    public struct MotionParameters
+    {
+        public Vector3 Velocity;
+        public float MaxSpeed;
+        public float Acceleration;
+        public float Inertia;
+        public float CurrentSpeed;
+    }
+}
