cac1cadff8c4d23782b649d1e825af46a552b6c9
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2013-09-28 15:00:56 +0200

    The player ship now stays in the game area.

--- Assets/Scripts/Objects/Mobiles/PlayerShip.cs
+++ Assets/Scripts/Objects/Mobiles/PlayerShip.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using EventHorizonGame;
 
 public class PlayerShip : Ship
 {
@@ -13,14 +14,22 @@ public class PlayerShip : Ship
         if (model == null)
             Debug.LogWarning("HarbingerOfDeath - LoadDefaultModel() - Null");
 
-        SetModel(model);
+        SetModel(model, EventHorizon.Instance.STARTING_POSITION);
         primary = gameObject.AddComponent<Gun>();
         primary.SetShip(this);
     }
 
     void LimitShipPositionWithinBoundaries()
     {
+        if (Model != null)
+        {
+            Vector3 pos = Model.transform.position;
+            Rect wb = Globals.GameArea;
 
+            float newX = Mathf.Clamp(pos.x, wb.x + Size.width / 2, wb.x + wb.width - Size.width / 2);
+            float newY = Mathf.Clamp(pos.y, wb.y + Size.height / 2, wb.y + wb.height - Size.height / 2);
+            Model.transform.position = new Vector3(newX, newY, 0);
+        }
     }
 
     public override string ToString()
