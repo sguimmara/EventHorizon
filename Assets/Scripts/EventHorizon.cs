78c270ee8b90bee832853271810640fc3cdd135e
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2013-09-28 14:58:57 +0200

    Added public variables to EventHorizon
    - USE_PLACEHOLDER : Tells the game to replace every mobile's material by
    the PLACEHOLDER material.
    - STARTING_POSITION : the starting position of the player's ship at the
    beginning of the level.
    
    InitializeAreaRects() takes the bounds of the 2 Areas (GameArea,
    SpawnArea) to compute their Rect and put them in Globals.

--- Assets/Scripts/EventHorizon.cs
+++ Assets/Scripts/EventHorizon.cs
@@ -4,29 +4,91 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 
-public class EventHorizon : MonoBehaviour
+namespace EventHorizonGame
 {
-    Player player;
-
-    List<Mobile> mobiles;
-
-    void Start()
+    public class EventHorizon : MonoBehaviour
     {
-        mobiles = new List<Mobile>();
-        player = gameObject.AddComponent<Player>();
-    }
+        Player player;
 
-    public void AddMobile(Mobile m)
-    {
-        if (mobiles.Contains(m))
-            Debug.LogWarning("EventHorizon - Le mobile " + m + " est déjà présent dans la liste");
+        [NonSerialized]
+        public static EventHorizon Instance;
 
-        else mobiles.Add(m);
+        List<Mobile> mobiles;
+
+        public GameObject SpawnArea;
+        public GameObject GameArea;
+
+        public bool USE_PLACEHOLDERS = false;
+        public Material PLACEHOLDER;
+
+        public Vector3 STARTING_POSITION;
+
+        void InitializeAreaRects()
+        {
+            if (SpawnArea)
+            {
+                Bounds b = SpawnArea.GetComponent<MeshRenderer>().bounds;
+                Globals.SpawnArea.x = b.min.x;
+                Globals.SpawnArea.y = b.min.y;
+                Globals.SpawnArea.width = b.max.x - b.min.x;
+                Globals.SpawnArea.height = b.max.y - b.min.y;
+            }
+            else Debug.LogError("EventHorizon - SpawnArea is null");
+
+            if (GameArea)
+            {
+                Bounds b = GameArea.GetComponent<MeshRenderer>().bounds;
+                Globals.GameArea.x = b.min.x;
+                Globals.GameArea.y = b.min.y;
+                Globals.GameArea.width = b.max.x - b.min.x;
+                Globals.GameArea.height = b.max.y - b.min.y;
+            }
+            else Debug.LogError("EventHorizon - GameArea is null");
+        }
+
+        void InitializeDebugSettings()
+        {
+            if (USE_PLACEHOLDERS)
+            {
+                MeshRenderer[] renderers = UnityEngine.Object.FindObjectsOfType(typeof(MeshRenderer)) as MeshRenderer[];
+
+                foreach (MeshRenderer r in renderers)
+                {
+                    for (int i = 0; i < r.materials.Length; i++)
+                    {
+                        r.materials[i] = PLACEHOLDER;
+                    }
+                }
+            }
+        }
+
+        void Awake()
+        {
+           
+        }
+
+        void Start()
+        {
+            Instance = this;
+            mobiles = new List<Mobile>();
+            player = gameObject.AddComponent<Player>();
+
+            InitializeAreaRects();
+            InitializeDebugSettings();
+        }
+
+        public void AddMobile(Mobile m)
+        {
+            if (mobiles.Contains(m))
+                Debug.LogWarning("EventHorizon - Le mobile " + m + " est déjà présent dans la liste");
+
+            else mobiles.Add(m);
+        }
+
+        void Update()
+        {
+
+        }
     }
-	
-		void Update()
-	{
-		
-	}
 }
 
