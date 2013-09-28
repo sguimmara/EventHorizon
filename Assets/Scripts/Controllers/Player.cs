29202630f44ff09f7dac3a9b6c759497a72cd221
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2013-09-28 19:48:10 +0200

    Player class changed
    Removed SetShip.

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -5,33 +5,18 @@ namespace EventHorizonGame
     public class Player : MonoBehaviour
     {
         PlayerShip ship;
-        EventHorizon eventHorizon;
-
-        public void SetShip<T>() where T : PlayerShip
-        {
-            if (ship)
-                Destroy(ship);
-
-            ship = gameObject.AddComponent<T>();
-            ship.LoadDefaultModel();
-            ship.motionParams = new MotionParameters { MaxSpeed = 10, Inertia = 0.9F, Acceleration = 2F, Velocity = Vector3.zero, CurrentSpeed = 0};
-        }
 
         // Use this for initialization
         void Start()
         {
-            eventHorizon = GetComponent<EventHorizon>();
-
-            SetShip<PlayerShip>();
-
-            eventHorizon.AddMobile(ship);
+            ship = Pool.Instance.Create<PlayerShip>("Player", EventHorizon.Instance.STARTING_POSITION, "Mobiles/Ships/Player/HarbingerOfDeath");
+            ship.motionParams = new MotionParameters { Acceleration = 1F, CurrentSpeed = 0f, Velocity = Vector3.zero, MaxSpeed = 10F, Inertia = 0.9F };
+            ship.data = new MobileData { damage = 0, hp = 5, isDestroyable = true };
         }
 
         // Update is called once per frame
         void Update()
         {
-            //ship.Stop();
-
             if (Input.GetKey(KeyCode.DownArrow))
             {
                 ship.Move(Vector3.down);
