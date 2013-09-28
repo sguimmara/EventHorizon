59b787603f1c3593ec3cb4514e8a05b9e5042fdd
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2013-09-28 18:39:18 +0200

    Added MobileData struct
    This stores the mobile's game information, like Hit points,
    isDestroyable.

---
+++ Assets/Scripts/Objects/Mobiles/MobileData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EventHorizonGame
+{
+    public struct MobileData
+    {
+        public bool isDestroyable;
+        public uint hp;
+        public uint damage;
+    }
+}
