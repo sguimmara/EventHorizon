4ca17a6f10987d9d99a8c6d9d4b86d54168897ba
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2013-10-03 01:25:05 +0200

    Added interfaces IControlable & ICollidable

---
+++ Unity/Assets/Scripts/Objects/Mobiles/ICollidable.cs
@@ -0,0 +1,21 @@
+﻿using EventHorizon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace EventHorizon
+{
+    public interface ICollidable
+    {
+        int CurrentHp { get; }
+        int MaxHp {get; set;}
+
+        void Destroy();
+        event EventMobile OnDestroy;
+        void UpdateHp();
+
+        void OnTriggerEnter(Collider other);
+    }
+}
--- Unity/Assets/Scripts/Objects/Mobiles/IControlable.cs
+++ Unity/Assets/Scripts/Objects/Mobiles/IControlable.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace EventHorizonGame
+namespace EventHorizon
 {
-    public interface IControlable
+    public interface IPlayable
     {
-        void UserControl();
+        void Control();
     }
 }
