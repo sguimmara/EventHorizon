4033e80cbc86c57f3695add15fc6b63efef6d734
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2013-10-03 20:51:23 +0200

    First draft of LevelSlider class

---
+++ Unity/Assets/Scripts/Controllers/LevelSlider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace EventHorizon
+{
+    public class LevelSlider : MonoBehaviour
+    {
+        void Update()
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * 4);
+        }
+    }
+}
