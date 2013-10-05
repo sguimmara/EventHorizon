7b587b2b7e6795d8ff8307077b68a93d76432df5
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2013-10-05 15:22:38 +0200

    Added score and upgrade screens
    draft

---
+++ Unity/Assets/Scripts/UI/ScoreScreen.cs
@@ -0,0 +1,20 @@
+﻿using EventHorizon.UserInterface;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.IO;
+using EventHorizon.Storyline;
+using EventHorizon.Core;
+
+namespace EventHorizon.UserInterface
+{
+    public class ScoreScreen : GuiRenderer
+    {
+        public static ScoreScreen Instance;
+
+        public event GameEvent OnScoreFinished;       
+    }
+}
