66f91de6d18eed93aa32ceeadb3cb47e2332ea9d
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2013-10-05 19:40:49 +0200

    Added functionality to Level class

--- Unity/Assets/Scripts/Engine/Level.cs
+++ Unity/Assets/Scripts/Engine/Level.cs
@@ -6,10 +6,33 @@ using UnityEngine;
 
 namespace EventHorizon.Core
 {
+    public enum LevelPhase { Init, Intro, Upgrade, Game, Score }
+
     public class Level
     {
-        string Name;
-        string CutsceneText;
-        AudioClip MainMusic; 
+        public int number;
+        public string Name;
+        public string IntroText;
+        AudioClip MainMusic;
+
+        public EventLevel OnLevelLoaded;
+
+        public void Load()
+        {
+            Application.LoadLevel(number);
+            Debug.Log("Loading level " + this.ToString());
+        }
+
+        public override string ToString()
+        {
+            return (string.Concat(number.ToString(), " ", Name));
+        }
+
+        public Level(string name, int number, string introText)
+        {
+            this.number = number;
+            this.Name = name;
+            this.IntroText = introText;
+        }
     }
 }
