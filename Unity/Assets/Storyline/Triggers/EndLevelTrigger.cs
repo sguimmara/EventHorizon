c2f249a2fe882191beedd4406fcb8a3172e20db4
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2013-10-05 15:23:29 +0200

    Added endlevel trigger & Trigger abstract class

---
+++ Unity/Assets/Storyline/Triggers/EndLevelTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.IO;
+using EventHorizon.Storyline;
+using EventHorizon.Core;
+
+namespace EventHorizon.Triggers
+{
+    public class EndLevelTrigger : TriggerBehaviour
+    {
+        protected override void Trigger()
+        {
+            Engine.Instance.ReachEndOfLevel();
+        }
+    }
+}
---
+++ Unity/Assets/Storyline/Triggers/Trigger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace EventHorizon.Triggers
+{
+    public enum TriggerType { Player }
+
+    public abstract class TriggerBehaviour : MonoBehaviour
+    {
+        public TriggerType type;
+
+        protected abstract void Trigger();
+
+        protected virtual void OnTriggerEnter(Collider other)
+        {
+            Debug.Log(other.tag + " " + type.ToString());
+            if (other.tag == type.ToString())
+                Trigger();
+        }
+    }
+}
