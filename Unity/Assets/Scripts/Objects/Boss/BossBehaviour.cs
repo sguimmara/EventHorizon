4ab9cb86fa647d974e56f7406f8d0c8bee4d597c
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2013-10-06 22:52:16 +0200

    Added boss prefab & behaviour

---
+++ Unity/Assets/Scripts/Objects/Boss/BossBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using EventHorizon.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace EventHorizon.Behaviour
+{
+    public class BossBehaviour : MonoBehaviour
+    {
+        protected virtual void OnDestroy()
+        {
+            LevelSlider.Instance.speed = 2;
+        }
+    }
+}
