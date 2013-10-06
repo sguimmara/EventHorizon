9fc2ad9f7c13775471c898f0f127ad35421e92fb
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2013-10-06 22:56:31 +0200

    Added shake to Camera controller

--- Unity/Assets/Scripts/Engine/CameraController.cs
+++ Unity/Assets/Scripts/Engine/CameraController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EventHorizon.Core;
+using EventHorizon.Objects;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,14 +12,43 @@ namespace EventHorizon
     public sealed class CameraController : MonoBehaviour
     {
         public static CameraController Instance { get; private set; }
+        Vector3 originalPosition;
 
         void Awake()
         {
             Instance = this;
+            originalPosition = transform.position;
         }
 
-        public void Shake()
+        public void Shake(Mobile ship)
         {
+            StopAllCoroutines();
+            StartCoroutine(ShakeSequence());
+        }
+
+        void Start()
+        {
+            Engine.Instance.OnShipDestroyed += Shake;
+        }
+
+        IEnumerator ShakeSequence()
+        {
+            yield return new WaitForEndOfFrame();
+            yield return new WaitForEndOfFrame();
+            yield return new WaitForEndOfFrame();
+
+
+            float f = 0;
+            float sign = 1;
+            while (f < 0.02F)
+            {
+                sign *= -1;
+                transform.position += (new Vector3(0.1F * sign, 0.1F * sign, transform.position.z));
+                f += Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+
+            transform.position = originalPosition;
 
         }
     }
