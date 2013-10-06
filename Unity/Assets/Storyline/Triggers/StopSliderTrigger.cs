5767e1c9a0d53b566ae9d16b974449dee8544660
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2013-10-06 22:51:51 +0200

    Added StopSliderTrigger

---
+++ Unity/Assets/Storyline/Triggers/StopSliderTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.IO;
+using EventHorizon.Storyline;
+using EventHorizon.Core;
+
+namespace EventHorizon.Triggers
+{
+    public class StopSliderTrigger : TriggerBehaviour
+    {
+        protected override void Trigger()
+        {
+            LevelSlider.Instance.speed = 0;
+        }
+    }
+}
