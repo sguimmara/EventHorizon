bf781f8d4dd51c585f634e64d3404260f4281ee5
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2013-10-08 00:41:07 +0200

    Added music trigger

---
+++ Unity/Assets/Storyline/Triggers/MusicTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.IO;
+using EventHorizon.Storyline;
+using EventHorizon.Core;
+
+namespace EventHorizon.Triggers
+{
+    public class MusicTrigger : TriggerBehaviour
+    {
+        public AudioClip Clip;
+        public bool Loop;
+
+        void Awake()
+        {
+            if (!audio)
+                gameObject.AddComponent<AudioSource>();
+        }
+
+        protected override void Trigger()
+        {
+            if (!Loop)
+                audio.PlayOneShot(Clip);
+
+            else
+            {
+                audio.clip = Clip;
+                audio.loop = Loop;
+                audio.Play();
+            }
+        }
+    }
+}
