f4a16669b6398186c959a8de2abf1a8435cdf86f
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2013-10-11 00:08:40 +0200

    Added EnemyShip class

---
+++ Unity/Assets/Scripts/Objects/Mobiles/EnemyShip.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using EventHorizon.Objects;
+using EventHorizon.Effects;
+using EventHorizon.Core;
+using EventHorizon.AI;
+
+namespace EventHorizon.Objects
+{
+    public class EnemyShip : Ship, ICollidable
+    {
+        public bool AutoTrigger;
+
+        public AIContainer AIBehaviours;
+
+        public override string ToString()
+        {
+            return "EnemyShip";
+        }
+
+        protected override void OnBecameVisible()
+        {
+            base.OnBecameVisible();
+
+            if (Engine.Instance != null)
+                Engine.Instance.AddShip(this);
+
+            if (AIBehaviours.motionPattern != null)
+                AIBehaviours.motionPattern.Create(this.transform);
+        }
+
+        void OnDrawGizmos()
+        {
+            if (!Application.isPlaying)
+                if (AIBehaviours.motionPattern != null)
+                    if (AIBehaviours.motionPattern.GetType() == typeof(FollowPath))
+                    {
+                        ((FollowPath)AIBehaviours.motionPattern).currentTransform = transform;
+                        ((FollowPath)AIBehaviours.motionPattern).OnDrawGizmos();
+                    }
+        }
+
+        public override void Trigger()
+        {
+            for (int i = 0; i < Slots.Length; i++)
+                Slots[i].Trigger();
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+            if (AutoTrigger)
+                Trigger();
+        }
+    }
+}
