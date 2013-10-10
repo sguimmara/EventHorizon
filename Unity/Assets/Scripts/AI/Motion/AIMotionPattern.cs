98516a193af4ac056d867570fffb269d5b7cccf1
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2013-10-11 00:07:09 +0200

    MotionPattern instance is not parented anymore

--- Unity/Assets/Scripts/AI/Motion/AIMotionPattern.cs
+++ Unity/Assets/Scripts/AI/Motion/AIMotionPattern.cs
@@ -6,17 +6,23 @@ using UnityEngine;
 
 namespace EventHorizon.AI
 {
-    public class AIMotionPattern : AIBehaviour, ICreatable
+    [Serializable]
+    public abstract class AIMotionPattern : AIBehaviour, ICreatable
     {
+        public bool Loop;
+
         [HideInInspector]
         public Transform objectToMove;
+
+        public abstract void Play();
 
         public void Create(Transform parent)
         {
             GameObject g = (GameObject) GameObject.Instantiate(gameObject, parent.position, parent.rotation);
             AIMotionPattern m = g.GetComponent<AIMotionPattern>();
             m.objectToMove = parent;
-            g.transform.parent = parent.root;
+            //g.transform.parent = parent.root;
+            m.Play();
         }
     }
 }
