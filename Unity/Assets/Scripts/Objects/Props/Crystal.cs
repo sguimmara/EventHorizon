4c8a410607e49d8542c871fa8481eb58764f7c2e
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2013-10-27 01:51:23 +0200

    Added crystal

---
+++ Unity/Assets/Scripts/Objects/Props/Crystal.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EventHorizon.Objects
+{
+    class Crystal : Mobile, ICollidable
+    {
+        public bool Activated { get; private set; }
+        private float originalShininess;
+        private GameObject glow;
+
+        private void Awake()
+        {
+            Material m = new Material(renderer.material);
+            renderer.material = m;
+            originalShininess = m.GetFloat("_Shininess");
+            glow = transform.Find("crystal glow").gameObject;
+            glow.SetActive(false);
+        }
+
+        public void OnTriggerEnter(Collider other)
+        {
+
+        }
+
+        public void Collide(ICollidable other)
+        {
+
+        }
+
+        public override void NotifyHitByLaser(LaserType type)
+        {
+            Activated = true;
+        }
+
+        void Update()
+        {
+            if (Activated)
+            {
+                renderer.material.SetFloat("_Shininess", 0);
+
+                if (!glow.activeSelf)
+                    glow.SetActive(true);
+            }
+
+
+            else
+            {
+                renderer.material.SetFloat("_Shininess", originalShininess);
+
+                if (glow.activeSelf)
+                    glow.SetActive(false);
+            }
+
+            Activated = false;
+        }
+    }
+}
