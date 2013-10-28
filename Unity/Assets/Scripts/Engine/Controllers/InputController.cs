642ec5ff11f38011e5c632a72075561badc474af
Sebastien Guimmara <sebastien.guimmara@gmail.com>
2013-10-29 00:02:43 +0100

    The mouse no longer controls the player

--- Unity/Assets/Scripts/Engine/Controllers/InputController.cs
+++ Unity/Assets/Scripts/Engine/Controllers/InputController.cs
@@ -10,34 +10,45 @@ namespace EventHorizon.Core
         public float depth;
         Vector3 originalRotation;
 
+        Vector3 originalMousePosition;
+        Vector3 delta;
+
         // Use this for initialization
         void Start()
         {
+            originalMousePosition = Input.mousePosition;
             originalRotation = transform.eulerAngles;
             Screen.showCursor = false;
+        }
+
+        void OnGUI()
+        {
+            string s = string.Format("{0} - {1}", originalMousePosition, delta);
+            GUI.Label(new Rect(0, 0, 300, 20), s);
         }
 
         // Update is called once per frame
         void Update()
         {
-             //Absolute position
-            worldPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, depth));
-            Vector3 velocity = new Vector3();
-            Vector3 result = Vector3.SmoothDamp(transform.position, worldPos, ref velocity, 0.1F);
-            transform.position = new Vector3(result.x, result.y, -1F) ;
+            //// Absolute position
+            //Vector3 newMousePosition = Input.mousePosition - originalMousePosition;
+            //delta = new Vector3(newMousePosition.x, newMousePosition.y, 0);
+            //originalMousePosition = Input.mousePosition;
+
+            //transform.Translate(delta / 100F, Space.World);
 
             float val = 1;
 
             // Relative position
             if (Input.GetKeyDown(KeyCode.DownArrow))
             {
-                transform.Translate(new Vector3(0, 0, -val));
+                transform.Translate(new Vector3(0, 0, -val/20F));
             }
 
             // Relative position
             if (Input.GetKeyDown(KeyCode.UpArrow))
             {
-                transform.Translate(new Vector3(0, 0, val));
+                transform.Translate(new Vector3(0, 0, val/20F));
             }
 
             if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
